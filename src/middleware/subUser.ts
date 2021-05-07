795d07a6cf44e3e2b2fd3712171a0e994d03ea4d
idris agboola <agboolaisholaidreez@gmail.com>
2021-05-07 11:29:30 +0100

    update

---
+++ src/middleware/subUser.ts
@@ -0,0 +1,30 @@
+import fs from "fs";
+import jwt from "jsonwebtoken";
+import { Sub } from "../entities/sub";
+
+const userSubMiddleware = async (req, res, next) => {
+  const type = req.body.type;
+  try {
+    const sub: Sub = await Sub.findOne({ name: req.params.name });
+
+    if (sub.username !== res.locals.user.username)
+      return res
+        .status(400)
+        .json({ image: "authorization denied,you don't own the sub" });
+
+    if (type !== "image" && type !== "banner") {
+      fs.unlinkSync(req.file.path);
+      return res.status(400).json({ type: "type must be image/banner" });
+    }
+
+    if (!req.file)
+      return res.status(400).json({ image: "only png/jpg format" });
+
+    res.locals.sub = sub;
+    next();
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+export default userSubMiddleware;
