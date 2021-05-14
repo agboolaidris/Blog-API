de8dbd83128b9f260319002bc86ff5206990c3e2
idris agboola <agboolaisholaidreez@gmail.com>
2021-05-14 19:49:15 +0100

    update

--- client/src/layout/index.tsx
+++ client/src/layout/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Header from "../components/Header/header";
+import Header from "../components/header/header";
 function Layout({ children }) {
   return (
     <div className="">
